3cbc1113fde7e79ca10afd2d30f66a03c39c4547
Travis Llado <travisllado@utexas.edu>
2018-06-27 17:56:02 -0500

    updated About, adding some member data

--- placeholderWebsite/assets/about.js
+++ placeholderWebsite/assets/about.js
@@ -1,12 +1,29 @@
-// $(document).ready(function(){
-    $.get("https://gitlab.com/api/v4/projects/7160520?private_token=eX7szajR1g6q1C9hyCr4", function(gitlabData){
-        $("#ourDescription").html(gitlabData.description);
-        $("#ourName").html(gitlabData.name);
-        $("#ourCreatedAt").html(gitlabData.created_at);
-        $("#ourURL").html(gitlabData.web_url);
-        $("#ourReadme").html(gitlabData.readme_url);
-        $("#ourStars").html(gitlabData.star_count);
-        $("#ourForks").html(gitlabData.forks_count);
-        $("#ourLastChange").html(gitlabData.last_activity_at);
-    });
-// });
+$(document).ready(function(){
+    // Get project information
+    $.get(
+        "https://gitlab.com/api/v4/projects/7160520?private_token=eX7szajR1g6q1C9hyCr4",
+        function(gitlabData) {
+            $("#ourDescription").html(gitlabData.description);
+            $("#ourName").html(gitlabData.name);
+            $("#ourCreatedAt").html(gitlabData.created_at);
+            $("#ourURL").html(gitlabData.web_url);
+            $("#ourReadme").html(gitlabData.readme_url);
+            $("#ourStars").html(gitlabData.star_count);
+            $("#ourForks").html(gitlabData.forks_count);
+            $("#ourLastChange").html(gitlabData.last_activity_at);
+        }
+    );
+    // Get team member information
+    $.get(
+        "https://gitlab.com/api/v4/projects/7160520/members?private_token=eX7szajR1g6q1C9hyCr4",
+        function(gitlabData) {
+            var names = [];
+            gitlabData.forEach(
+                function(element) {
+                    names.push(element.name);
+                }
+            );
+            $("#memberNames").html(names);
+        }
+    );
+});
