ebf207d6935eb22a0b00c092191871f50d944137
Beaudry Chase <beauchase213@gmail.com>
2018-07-12 17:34:41 -0500

    Refactored test.py
    refactored test.py and added more tests.

--- website_backend/app/test.py
+++ website_backend/app/test.py
@@ -14,42 +14,63 @@ from database import db as db
 class FlaskrTestCase(unittest.TestCase):
 
 	def setUp(self):
+		db.session.rollback()
 		#Assumes the database has already been initialized.
-		
-		print("hello")
-
-	def test_diseases(self):
-		sample_disease =   {"name" : "Sample Disease : WHO", 
-							"symptoms" : "Sample Symptoms",
-							"transmission" : "Sample Transmission",
-							"diagnosis": "Sample Diagnosis",
-							"treatment" : "Sample Treatment",
-							"prevention" : "Sample Prevention",
-							"more" : "Sample More",
+		sample_disease =   {"name" : "Sample disease : WHO", 
+							"symptoms" : "Sample symptoms",
+							"transmission" : "Sample transmission",
+							"diagnosis": "Sample diagnosis",
+							"treatment" : "Sample treatment",
+							"prevention" : "Sample prevention",
+							"more" : "Sample more",
 							"is_active" : True
 							}
 		sample_disease = json.dumps(sample_disease)
 		sample_disease_json = json.loads(sample_disease)
-
-		
 		try:
 			db.session.add( database.Disease(sample_disease_json))
 		except IntegrityError:
-		 	db.session.rollback()
+			pass
+			db.session.rollback()
 
-		sample = db.session.query(database.Disease).filter_by(name="Sample Disease").one()  # filter on name
+	def test_disease_name(self):		
+		sample = db.session.query(database.Disease).filter_by(name="Sample disease").one()  # filter on name
+		assert "Sample disease" == sample.name
 
-		test_result = "Sample Disease" == sample.name
-		db.session.delete(sample)
-		#db.session.commit()
+	def test_disease_symptoms(self):		
+		sample = db.session.query(database.Disease).filter_by(symptoms="Sample symptoms").one()  # filter on name
+		assert "Sample symptoms" == sample.symptoms
 
-		assert "Sample Disease" == sample.name
+	def test_disease_transmission(self):		
+		sample = db.session.query(database.Disease).filter_by(transmission="Sample transmission").one()  # filter on name
+		assert "Sample transmission" == sample.transmission
+
+	def test_disease_diagnosis(self):		
+		sample = db.session.query(database.Disease).filter_by(diagnosis="Sample diagnosis").one()  # filter on name
+		assert "Sample diagnosis" == sample.diagnosis
+
+	def test_disease_treatment(self):		
+		sample = db.session.query(database.Disease).filter_by(treatment="Sample treatment").one()  # filter on name
+		assert "Sample treatment" == sample.treatment
+
+	def test_disease_prevention(self):		
+		sample = db.session.query(database.Disease).filter_by(prevention="Sample prevention").one()  # filter on name
+		assert "Sample prevention" == sample.prevention
+
+	def test_disease_more(self):		
+		sample = db.session.query(database.Disease).filter_by(more="Sample more").one()  # filter on name
+		assert "Sample more" == sample.more
+	
+	def test_disease_is_active(self):		
+		sample = db.session.query(database.Disease).filter_by(is_active=True, name="Sample disease").one()  # filter on name
+		assert sample.is_active
+	
 
 if __name__ == '__main__':
 	#app_file.initialize()
 	#print('app.Disease.query.all()')
 	db.create_all()
-	print([x.name for x in database.Disease.query.all()])
+	#print([x.name for x in database.Disease.query.all()])
 	unittest.main()
 	
 
