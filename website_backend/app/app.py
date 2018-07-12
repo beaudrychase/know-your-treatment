0ad0832017eabfda66a98420b5a552ff1644bf40
cmibarnwell <calebbarnwell@gmail.com>
2018-07-12 18:14:32 -0500

    forgot to say deploy

--- website_backend/app/app.py
+++ website_backend/app/app.py
@@ -27,6 +27,7 @@ charity_blueprint = manager.create_api(database.Charity, methods=['GET'])
 treatment_blueprint = manager.create_api(database.Treatment, methods=['GET'])
 
 if __name__ == "__main__":
+    # initialize
     initialize()
     app.run(host="0.0.0.0", port=8080, use_reloader=False)
     # Make available to all domains
