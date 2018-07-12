8f1eb843a024481ace71aceba347abf09243b245
brendan miller <brendanmiller.ca@gmail.com>
2018-07-12 02:24:39 -0500

    all tables fully populated with relations between objects

--- website_backend/app/app.py
+++ website_backend/app/app.py
@@ -4,23 +4,25 @@ from database import app as app
 from database import db as db
 import database
 
-db.create_all()
-db.session.commit()
-#database.initDisease()
-#database.initCharity()
-database.initTreatment()
+def initialize():
+	database.clearDB()
+	db.create_all()
+	db.session.commit()
+	database.initDisease()
+	database.initCharity()
+	database.initTreatment()
+	print('all tables initialized')
 
+initialize()
 
 @app.route('/')
 def hello_world():
     return 'go to /api/[disease/charity/treatment] to see our api endpoints'
 
-
 manager = flask_restless.APIManager(app, flask_sqlalchemy_db=database.db)
 disease_blueprint = manager.create_api(database.Disease, methods=['GET'])
 charity_blueprint = manager.create_api(database.Charity, methods=['GET'])
 treatment_blueprint = manager.create_api(database.Treatment, methods=['GET'])
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
--- website_backend/app/database.py
+++ website_backend/app/database.py
@@ -12,7 +12,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
-
 # for many-to-many relations from diseases to various treatments
 Disease_Treatment = db.Table('disease_treatment',
     db.Column('treatment_id', db.Integer, db.ForeignKey('treatment.id'), primary_key=True),
@@ -24,12 +23,13 @@ class Treatment(db.Model):
     name = db.Column(db.Unicode, unique=True)
     treatment_type = db.Column(db.Unicode)
     text = db.Column(db.Unicode)
+    wiki_link = db.Column(db.Unicode)
 
     def __init__(self, resource):
         self.name = resource['name']
         self.treatment_type = resource['treatment_type']
-        self.info = resource['text']
-
+        self.text = resource['text']
+        self.wiki_link = 'https://en.wikipedia.org/wiki/' + resource['name'].replace(' ','%20')
 
 class Charity(db.Model):
     ein = db.Column(db.Integer, primary_key=True)
@@ -78,7 +78,6 @@ class Charity(db.Model):
 class Disease(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Unicode, unique=True)
-    #facts = db.Column(db.Unicode)
     symptoms = db.Column(db.Unicode)
     transmission = db.Column(db.Unicode)
     diagnosis = db.Column(db.Unicode)
@@ -86,14 +85,12 @@ class Disease(db.Model):
     prevention = db.Column(db.Unicode)
     more = db.Column(db.Unicode)
     is_active = db.Column(db.Boolean)
-    wiki = db.Column(db.Unicode)
     charities = db.relationship('Charity', backref='disease', lazy=True)
     treatments = db.relationship('Treatment', secondary=Disease_Treatment, lazy='select',
     backref=db.backref('diseases', lazy=True))
 
     def __init__(self, resource):
         self.name = resource['name'][: -6] #the -6 removes ' : WHO' from the end of the name
-        #self.facts = resource['facts']
         self.symptoms = resource['symptoms']
         self.transmission = resource['transmission']
         self.diagnosis = resource['diagnosis']
@@ -101,13 +98,6 @@ class Disease(db.Model):
         self.prevention = resource['prevention']
         self.more = resource['more']
         self.is_active = resource['is_active']
-        self.wiki = ''
-
-        #url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + self.name
-        #data = simplejson.load(urlopen(url))
-        #The problem is that the page has a unique number that we need to get to access the
-        #nested dict, this finds the number and then uses it to get the contents of that key.
-        #self.wiki = data['query']['pages'][next(iter(data['query']['pages']))]['extract']
 
 def initDisease():
     url = 'https://disease-info-api.herokuapp.com/diseases.json'
@@ -119,6 +109,8 @@ def initDisease():
         except IntegrityError:
             db.session.rollback()
             # in case of adding duplicates
+    print('initialized disease table')
+
 
 def initCharity():
     baseUrl = 'http://data.orghunter.com/v1/charitysearch?user_key=5090f8b7b0c373370039798d01066edf&rows=2&searchTerm='
@@ -131,17 +123,18 @@ def initCharity():
             except IntegrityError:
                 db.session.rollback()
                 # in case of adding duplicates
+    print('initialized charity table')
+
 
 def initTreatment():
+    baseUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&rvsection=0&rvparse&titles='
+    baseSearchUrl = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=&srsearch='
     for disease in Disease.query.all():
-        print(disease.name, '------------------------')
         # query wikipedia for a given condition  
-        searchUrl = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=&srsearch='+(disease.name).replace(' ','%20')
-        print(searchUrl)
-        disease_wiki_title = json.load(urlopen(searchUrl))['query']['search'][0]['title'].replace(' ', '%20')
-
-        baseUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&rvsection=0&rvparse&titles='+disease_wiki_title
-        data = json.load( urlopen(baseUrl) )
+        searchUrl = baseSearchUrl+(disease.name).replace(' ','%20')
+        disease_wiki_title = requests.get(searchUrl).json()['query']['search'][0]['title'].replace(' ', '%20')
+        # access the page
+        data = json.load( urlopen(baseUrl+disease_wiki_title) )
         textData = (next(iter(data['query']['pages'].values()))['revisions'][0]['*'])
         textList = textData.split('<tr>')
         # iterate through the wikipedia infobox, grab only certain fields
@@ -151,32 +144,37 @@ def initTreatment():
                 # if there are links to other wiki pages
                 text = ''
                 if '/wiki/' in section:
-                    links = (section.split('<a href='))
-                    print(links)
+                    #links = (section.split('<a href='))
+                    #for link in links:
+                    #print(link.split)
 
                     suffix = section.split('/wiki/')[1].split('"')[0]
                     sectionUrl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='+suffix
-                    print('sectionUrl', sectionUrl)
                     text = next(iter(json.load( urlopen(sectionUrl) )['query']['pages'].values()))['extract']
-                # otherwise
-                #else:
-                    #text = section.split('</th><td>\n')[1].split('<')[0]
-                    #print(text)
+                    # in case the link requires a redirect/is a subpage
+                    if (text==''):
+                        sectionSearchUrl = baseSearchUrl+suffix
+                        treatment_wiki_title = json.load(urlopen(searchUrl))['query']['search'][0]['title'].replace(' ', '%20')
+                        text = next(iter(json.load( urlopen('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='+treatment_wiki_title) )['query']['pages'].values()))['extract']
 
                     if (text!=''):
                         try:
-                            info = {'name':'',
+                            info = {'name':suffix.replace('_',' '),
                                     'treatment_type':sectionName,
-                                    'text':text,
-                                    'wiki_link':suffix}
-                            #db.session.add( Treatment(info) )
-                            #db.session.commit()
+                                    'text':text}
+                            db.session.add( Treatment(info) )
+                            db.session.commit()
                         except IntegrityError:
-                            pass
+                            print('IntegrityError', info['name'])
                             db.session.rollback()
                             # in case of adding duplicates
+                        disease.treatments = disease.treatments + [Treatment.query.filter_by(name=suffix.replace('_',' ')).first()]
+                        db.session.commit()
+
+    print('initialized treatment table with', j, 'objects')
    
 
 def clearDB():
     db.reflect()
     db.drop_all()
+    print('cleared all tables')
