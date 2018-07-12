ba59f6b8a7bd280138cbb1853921cc6343c001d2
Travis Llado <travisllado@utexas.edu>
2018-07-12 14:47:24 -0500

    added href links to homepage because I don't know how to do react <Link>s

--- website_frontend/app/components/Home.js
+++ website_frontend/app/components/Home.js
@@ -5,23 +5,23 @@ const staticContent = (
         <div>
             <h1 class="display-3">Help Stop Health Problems Today.</h1>
             <p>Understand how you can help current health conditions. Browse our records for information on health conditions, charities, and medications to learn how you can help make the world a better place. </p>
-            <p>Link to /charities goes here</p>
+            <p><a href="charities">Find Charities Now</a></p>
         </div>
         <div class="row">
             <div class="col-md-4">
                 <h2>Health Conditions</h2>
                 <p>Learn more about current health conditions.</p>
-                <p><a href="healthconditions">Health Conditions</a></p>
+                <p><a href="healthconditions">View Details</a></p>
             </div>
             <div class="col-md-4">
                 <h2>Charities</h2>
                 <p>Explore which organizations are working to support health problems.</p>
-                <p>Link to /charities goes here</p>
+                <p><a href="charities">View Details</a></p>
             </div>
             <div class="col-md-4">
                 <h2>Medications</h2>
                 <p>Learn more about the medications and treatments available.</p>
-                <p>Link to /medications goes here</p>
+                <p><a href="medications">View Details</a></p>
             </div>
         </div>
     </div>
