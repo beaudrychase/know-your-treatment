3e1f2318bf0c03252853c2566b40a23430902049
Travis Llado <travisllado@utexas.edu>
2018-07-12 18:01:21 -0500

    got more of About page working

--- website_frontend/app/components/ListModule.js
+++ website_frontend/app/components/ListModule.js
@@ -2,34 +2,28 @@ import React from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
 export default class ListModule extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			names: []
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            names: []
+        };
+    }
 
-	componentWillMount() {
-		fetch('http://api.knowyourtreatment.com/api/disease')
-		.then(results => {
-			return results.json();
-		}).then(data => {
-			this.setState({names: data.objects.map((d) => {
-				console.log(d.name);
-				return d.name;
-			})});
-			/*console.log(this.state.names);*/
-		})
-		/*this.setState({names: nameArr});*/
-	}
+    componentWillMount() {
+        fetch('http://api.knowyourtreatment.com/api/disease')
+        .then(results => results.json())
+        .then(apiData => this.setState({
+            names: apiData.objects.map((element) => element.name)
+        }));
+    }
 
-	render() {
-		return(
-			<ListGroup>
-				{this.state.names.map(function(name) {
-					return <ListGroupItem> { name } </ListGroupItem>;
-				})}
-			</ListGroup>
-		);
-	}
+    render() {
+        return(
+            <ListGroup>
+                {this.state.names.map(function(element) {
+                    return <ListGroupItem> {element} </ListGroupItem>;
+                })}
+            </ListGroup>
+        );
+    }
 }
