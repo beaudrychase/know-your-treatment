b253f6eed565d3f2f34a8853565c988cb95803ea
cmibarnwell <calebbarnwell@gmail.com>
2018-07-12 18:23:52 -0500

    Trying this deploy

--- website_backend/app/app.py
+++ website_backend/app/app.py
@@ -5,6 +5,9 @@ from database import app as app
 from database import db as db
 import database
 
+
+
+
 def initialize():
 	database.clearDB()
 	db.create_all()
@@ -20,15 +23,13 @@ def home_page():
    			'/api/charity/  --  ' + str([x.charityName for x in database.Charity.query.all()]) + '<br>' +\
     		'/api/treatment/  --  ' + str([x.name for x in database.Treatment.query.all()])
 
-
-manager = flask_restless.APIManager(app, flask_sqlalchemy_db=database.db)
-disease_blueprint = manager.create_api(database.Disease, methods=['GET'])
-charity_blueprint = manager.create_api(database.Charity, methods=['GET'])
-treatment_blueprint = manager.create_api(database.Treatment, methods=['GET'])
-
 if __name__ == "__main__":
-    # initialize
-    initialize()
-    app.run(host="0.0.0.0", port=8080, use_reloader=False)
-    # Make available to all domains
+    app = Flask(__name__)
     CORS(app)
+    initialize()
+    manager = flask_restless.APIManager(app, flask_sqlalchemy_db=database.db)
+    disease_blueprint = manager.create_api(database.Disease, methods=['GET'])
+    charity_blueprint = manager.create_api(database.Charity, methods=['GET'])
+    treatment_blueprint = manager.create_api(database.Treatment, methods=['GET'])
+    # Make available to all domains
+    app.run(host="0.0.0.0", port=8080, use_reloader=False)
