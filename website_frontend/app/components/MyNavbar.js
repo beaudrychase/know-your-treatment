8e59017551e992ddaa28cb4d47f43bfcee036f35
Travis Llado <travisllado@utexas.edu>
2018-07-12 14:41:44 -0500

    updating homepage

--- website_frontend/app/components/MyNavbar.js
+++ website_frontend/app/components/MyNavbar.js
@@ -6,30 +6,23 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 
 export default class MyNavbar extends React.Component {
-	render() {
-		return (
-                        <Navbar color="dark" light expand = "md">
-
-                        <NavbarBrand href="/"> 
-                                <strong>Know Your Treatment</strong> 
-                        </NavbarBrand>
-
-                        <NavItem active>
-                                <Link to="/charities">Charities</Link>
-                        </NavItem>
-
-                        <NavItem active>
-                                <Link to="/healthconditions">Health Conditions</Link>
-                        </NavItem>
-
-                        <NavItem active>
-                                <Link to="/medications">Medications</Link>
-                        </NavItem>
-
-                        <NavItem active>
-                                <Link to="/about">About</Link>
-                        </NavItem>
-                        </Navbar>
-		);
-	}
+	render() {return(
+        <Navbar color="dark" light expand = "md">
+            <NavbarBrand href="/"> 
+                <strong>Know Your Treatment</strong> 
+            </NavbarBrand>
+            <NavItem active>
+                <Link to="/charities">Charities</Link>
+            </NavItem>
+            <NavItem active>
+                <Link to="/healthconditions">Health Conditions</Link>
+            </NavItem>
+            <NavItem active>
+                <Link to="/medications">Medications</Link>
+            </NavItem>
+            <NavItem active>
+                <Link to="/about">About</Link>
+            </NavItem>
+        </Navbar>
+	);}
 }
