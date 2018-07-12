f35928a3a0a80796ef6baf51f941985e06a73fd1
Unknown <csauce@utexas.edu>
2018-07-12 04:40:57 -0500

    added disease list
    Queries backend for disease list -- no interaction yet

--- website_frontend/app/components/ListModule.js
+++ website_frontend/app/components/ListModule.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 export default class ListModule extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			names: []
+		};
+	}
+
+	componentWillMount() {
+		fetch('https://disease-info-api.herokuapp.com/diseases.json')
+		.then(results => {
+			return results.json();
+		}).then(data => {
+			this.setState({names: data.diseases.map((d) => {
+				/*console.log(d.name);*/
+				return d.name;
+			})});
+			/*console.log(this.state.names);*/
+		})
+		/*this.setState({names: nameArr});*/
+	}
+
 	render() {
-		return <h1> ListModule </h1>;
+		return(
+			<ListGroup>
+				{this.state.names.map(function(name) {
+					return <ListGroupItem> { name } </ListGroupItem>;
+				})}
+			</ListGroup>
+		);
 	}
 }
