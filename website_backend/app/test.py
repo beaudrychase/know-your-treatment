7c4a3b5d042a1614fcfd50bd30f14fa91d0a3bdc
Beaudry Chase <beauchase213@gmail.com>
2018-07-12 21:39:51 -0500

    Improved test.py
    I changed the asserts in the tests to assertEqual

--- website_backend/app/test.py
+++ website_backend/app/test.py
@@ -22,35 +22,35 @@ class AppTestCase(unittest.TestCase):
 
 	def test_disease_name(self):		
 		sample = db.session.query(database.Disease).filter_by(name="Sample disease").one()  # filter on name
-		assert "Sample disease" == sample.name
+		self.assertEqual( "Sample disease", sample.name)
 
 	def test_disease_symptoms(self):		
 		sample = db.session.query(database.Disease).filter_by(symptoms="Sample symptoms").one()  # filter on name
-		assert "Sample symptoms" == sample.symptoms
+		self.assertEqual( "Sample symptoms", sample.symptoms)
 
 	def test_disease_transmission(self):		
 		sample = db.session.query(database.Disease).filter_by(transmission="Sample transmission").one()  # filter on name
-		assert "Sample transmission" == sample.transmission
+		self.assertEqual( "Sample transmission", sample.transmission)
 
 	def test_disease_diagnosis(self):		
 		sample = db.session.query(database.Disease).filter_by(diagnosis="Sample diagnosis").one()  # filter on name
-		assert "Sample diagnosis" == sample.diagnosis
+		self.assertEqual( "Sample diagnosis", sample.diagnosis)
 
 	def test_disease_treatment(self):		
 		sample = db.session.query(database.Disease).filter_by(treatment="Sample treatment").one()  # filter on name
-		assert "Sample treatment" == sample.treatment
+		self.assertEqual( "Sample treatment", sample.treatment)
 
 	def test_disease_prevention(self):		
 		sample = db.session.query(database.Disease).filter_by(prevention="Sample prevention").one()  # filter on name
-		assert "Sample prevention" == sample.prevention
+		self.assertEqual( "Sample prevention", sample.prevention)
 
 	def test_disease_more(self):		
 		sample = db.session.query(database.Disease).filter_by(more="Sample more").one()  # filter on name
-		assert "Sample more" == sample.more
+		self.assertEqual( "Sample more", sample.more)
 	
 	def test_disease_is_active(self):		
 		sample = db.session.query(database.Disease).filter_by(is_active=True, name="Sample disease").one()  # filter on name
-		assert sample.is_active
+		self.assertEqual(True , sample.is_active)
 
 
 
@@ -58,75 +58,75 @@ class AppTestCase(unittest.TestCase):
 
 	def test_charity_ein(self):
 		sample = db.session.query(database.Charity).filter_by(ein=1).one()
-		assert 1 == sample.ein
+		self.assertEqual( 1, sample.ein)
 
 	def test_charity_charityName(self):
 		sample = db.session.query(database.Charity).filter_by(charityName="Sample charityName").one()
-		assert "Sample charityName" == sample.charityName
+		self.assertEqual( "Sample charityName", sample.charityName)
 
 	def test_charity_url(self):
 		sample = db.session.query(database.Charity).filter_by(url="Sample url").one()
-		assert "Sample url" == sample.url
+		self.assertEqual( "Sample url", sample.url)
 
 	def test_charity_donationUrl(self):
 		sample = db.session.query(database.Charity).filter_by(donationUrl="Sample donationUrl").one()
-		assert "Sample donationUrl" == sample.donationUrl
+		self.assertEqual( "Sample donationUrl", sample.donationUrl)
 
 	def test_charity_city(self):
 		sample = db.session.query(database.Charity).filter_by(city="Sample city").one()
-		assert "Sample city" == sample.city
+		self.assertEqual( "Sample city", sample.city)
 
 	def test_charity_state(self):
 		sample = db.session.query(database.Charity).filter_by(state="Sample state").one()
-		assert "Sample state" == sample.state
+		self.assertEqual( "Sample state", sample.state)
 
 	def test_charity_zipCode(self):
 		sample = db.session.query(database.Charity).filter_by(zipCode="Sample zipCode").one()
-		assert "Sample zipCode" == sample.zipCode
+		self.assertEqual( "Sample zipCode", sample.zipCode)
 
 	def test_charity_start(self):
 		sample = db.session.query(database.Charity).filter_by(start=2).one()
-		assert 2 == sample.start
+		self.assertEqual( 2, sample.start)
 
 	def test_charity_rows(self):
 		sample = db.session.query(database.Charity).filter_by(rows=3).one()
-		assert 3 == sample.rows
+		self.assertEqual( 3, sample.rows)
 
 	def test_charity_recordCount(self):
 		sample = db.session.query(database.Charity).filter_by(recordCount=4).one()
-		assert 4 == sample.recordCount
+		self.assertEqual( 4, sample.recordCount)
 
 	def test_charity_score(self):
 		sample = db.session.query(database.Charity).filter_by(score=5,  charityName="Sample charityName").one()
-		assert 5 == sample.score
+		self.assertEqual( 5, sample.score)
 
 	def test_charity_acceptingDonations(self):
 		sample = db.session.query(database.Charity).filter_by(acceptingDonations=True, charityName="Sample charityName").one()
-		assert sample.acceptingDonations
+		self.assertEqual(True, sample.acceptingDonations)
 
 	def test_charity_category(self):
 		sample = db.session.query(database.Charity).filter_by(category="Sample category").one()
-		assert "Sample category" == sample.category
+		self.assertEqual( "Sample category", sample.category)
 
 	def test_charity_eligibleCd(self):
 		sample = db.session.query(database.Charity).filter_by(eligibleCd=True,  charityName="Sample charityName").one()
-		assert sample.eligibleCd
+		self.assertEqual(True, sample.eligibleCd)
 
 	def test_charity_missionStatement(self):
 		sample = db.session.query(database.Charity).filter_by(missionStatement="Sample missionStatement").one()
-		assert "Sample missionStatement" == sample.missionStatement
+		self.assertEqual( "Sample missionStatement", sample.missionStatement)
 
 	def test_charity_parent_ein(self):
 		sample = db.session.query(database.Charity).filter_by(parent_ein=True,  charityName="Sample charityName").one()
-		assert sample.parent_ein
+		self.assertEqual(True, sample.parent_ein)
 
 	def test_charity_longitude(self):
 		sample = db.session.query(database.Charity).filter_by(longitude=1.0).one()
-		assert 1.0 == sample.longitude
+		self.assertEqual( 1.0, sample.longitude)
 
 	def test_charity_latitude(self):
 		sample = db.session.query(database.Charity).filter_by(latitude=2.0).one()
-		assert 2.0 == sample.latitude
+		self.assertEqual( 2.0, sample.latitude)
 
 
 
@@ -134,23 +134,24 @@ class AppTestCase(unittest.TestCase):
 
 	def test_treatment_id(self):
 		sample = db.session.query(database.Treatment).filter_by(id=1).one()
-		assert 1 == sample.id
+		self.assertEqual( 1, sample.id)
 
 	def test_treatment_name(self):
 		sample = db.session.query(database.Treatment).filter_by(name='Sample name').one()
-		assert 'Sample name' == sample.name
+		self.assertEqual( 'Sample name', sample.name)
 
 	def test_treatment_treatment_type(self):
 		sample = db.session.query(database.Treatment).filter_by(treatment_type='Sample treatment_type').one()
-		assert 'Sample treatment_type' == sample.treatment_type
+		self.assertEqual( 'Sample treatment_type', sample.treatment_type)
 
 	def test_treatment_text(self):
 		sample = db.session.query(database.Treatment).filter_by(text='Sample text').one()
-		assert 'Sample text' == sample.text
+		self.assertEqual( 'Sample text', sample.text)
 
 	def test_treatment_wiki_link(self):
 		sample = db.session.query(database.Treatment).filter_by(wiki_link='https://en.wikipedia.org/wiki/Sample%20name').one()
-		assert 'https://en.wikipedia.org/wiki/Sample%20name' == sample.wiki_link
+		self.assertEqual( 'https://en.wikipedia.org/wiki/Sample%20name', sample.wiki_link)
+
 
 
 	
@@ -173,6 +174,7 @@ def create_sample_disease():
 	except IntegrityError:
 		pass
 		db.session.rollback()
+	return sample_disease_json
 
 def create_sample_charity():
 	sample_charity={'ein' : 1,
@@ -201,6 +203,8 @@ def create_sample_charity():
 	except IntegrityError:
 		pass
 		db.session.rollback()
+	return sample_charity_json
+
 
 def create_sample_treatment():
 	sample_treatment = {'id' : 1,
@@ -216,6 +220,7 @@ def create_sample_treatment():
 	except IntegrityError:
 		pass
 		db.session.rollback()
+	return sample_treatment_json
 
 if __name__ == '__main__':
 	#app_file.initialize()
