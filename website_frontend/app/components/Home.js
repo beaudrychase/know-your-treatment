fc114c98a4fefa7bc276783c29e43370ff020891
Travis Llado <travisllado@utexas.edu>
2018-07-13 02:48:54 -0500

    fixed homepage links

--- website_frontend/app/components/Home.js
+++ website_frontend/app/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends React.Component {
     render() {return(
@@ -6,23 +7,23 @@ export default class Home extends React.Component {
             <div>
                 <h1 class="display-3">Help Stop Health Problems Today.</h1>
                 <p>Understand how you can help current health conditions. Browse our records for information on health conditions, charities, and medications to learn how you can help make the world a better place. </p>
-                <p><a href="charities">Find Charities Now</a></p>
+                <Link to="/charities">Find charities now</Link>
             </div>
             <div class="row">
                 <div class="col-md-4">
                     <h2>Health Conditions</h2>
                     <p>Learn more about current health conditions.</p>
-                    <p><a href="healthconditions">View Details</a></p>
+                    <Link to="/healthconditions">View Details</Link>
                 </div>
                 <div class="col-md-4">
                     <h2>Charities</h2>
                     <p>Explore which organizations are working to support health problems.</p>
-                    <p><a href="charities">View Details</a></p>
+                    <Link to="/charities">View Details</Link>
                 </div>
                 <div class="col-md-4">
                     <h2>Medications</h2>
                     <p>Learn more about the medications and treatments available.</p>
-                    <p><a href="medications">View Details</a></p>
+                    <Link to="/medications">View Details</Link>
                 </div>
             </div>
             <footer class="container">
