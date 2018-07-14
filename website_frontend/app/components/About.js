09d66e49ec8357fdacf255d681881ba47294455b
Travis Llado <travisllado@utexas.edu>
2018-07-13 20:53:24 -0500

    updated # tests

--- website_frontend/app/components/About.js
+++ website_frontend/app/components/About.js
@@ -64,7 +64,7 @@ export default class About extends React.Component {
             ourLastChange:  "",
             numCommits:     [0, 8, 0, 0, 0, 0],
             numIssues:      [0, 0, 0, 0, 0, 0],
-            numTests:       [0, 0, 0, 12, 34, 0]
+            numTests:       [0, 9, 0, 12, 34, 0]
         };
     }
 
