e1b89c055b8235f91bbf5dd4212beeafbd65c2d8
woojunan <woo.an31@live.com>
2018-07-18 15:22:40 -0500

    added pagination tests

--- website_frontend/guitests.py
+++ website_frontend/guitests.py
@@ -19,99 +19,99 @@ class GUI_tests(unittest.TestCase) :
 		self.assertEqual(websiteURL, driver.current_url)
 		driver.close()
 
-	# # Tests the title of the home page
-	# def test_title(self) :
-	# 	driver = self.driver
-	# 	self.assertEqual('Know Your Treatment', driver.title)
-	# 	driver.close()
+	# Tests the title of the home page
+	def test_title(self) :
+		driver = self.driver
+		self.assertEqual('Know Your Treatment', driver.title)
+		driver.close()
 
 
-	# # Tests the home page button
-	# def test_home_button(self) :
-	# 	websiteURL = self.websiteURL
-	# 	driver = self.driver
-	# 	driver.find_element_by_link_text('Know Your Treatment').click()
-	# 	self.driver.implicitly_wait(30)
-	# 	self.assertEqual(websiteURL, driver.current_url)
-	# 	driver.close()
+	# Tests the home page button
+	def test_home_button(self) :
+		websiteURL = self.websiteURL
+		driver = self.driver
+		driver.find_element_by_link_text('Know Your Treatment').click()
+		self.driver.implicitly_wait(30)
+		self.assertEqual(websiteURL, driver.current_url)
+		driver.close()
 
-	# # Tests the links in the navbar
-	# def test_navbar_links(self) :
-	# 	websiteURL = self.websiteURL
-	# 	driver = self.driver
-	# 	link = driver.find_element_by_link_text('Charities')
-	# 	link.click()
-	# 	self.assertEqual(websiteURL + "charities", driver.current_url)
-	# 	link = driver.find_element_by_link_text('Health Conditions')
-	# 	link.click()
-	# 	self.driver.implicitly_wait(30)
-	# 	self.assertEqual(websiteURL + "healthconditions", driver.current_url)
-	# 	link = driver.find_element_by_link_text('Medications')
-	# 	link.click()
-	# 	self.driver.implicitly_wait(30)
-	# 	self.assertEqual(websiteURL + "medications", driver.current_url)
-	# 	link = driver.find_element_by_link_text('About')
-	# 	link.click()
-	# 	self.driver.implicitly_wait(30)
-	# 	self.assertEqual(websiteURL + "about", driver.current_url)
-	# 	driver.close()
+	# Tests the links in the navbar
+	def test_navbar_links(self) :
+		websiteURL = self.websiteURL
+		driver = self.driver
+		link = driver.find_element_by_link_text('Charities')
+		link.click()
+		self.assertEqual(websiteURL + "charities", driver.current_url)
+		link = driver.find_element_by_link_text('Health Conditions')
+		link.click()
+		self.driver.implicitly_wait(30)
+		self.assertEqual(websiteURL + "healthconditions", driver.current_url)
+		link = driver.find_element_by_link_text('Medications')
+		link.click()
+		self.driver.implicitly_wait(30)
+		self.assertEqual(websiteURL + "medications", driver.current_url)
+		link = driver.find_element_by_link_text('About')
+		link.click()
+		self.driver.implicitly_wait(30)
+		self.assertEqual(websiteURL + "about", driver.current_url)
+		driver.close()
 
-	# # Tests the links on the home page
-	# def test_homepage_links(self) :
-	# 	websiteURL = self.websiteURL
-	# 	driver = self.driver
-	# 	link = driver.find_element_by_link_text('Find charities now')
-	# 	link.click()
-	# 	self.driver.implicitly_wait(30)
-	# 	self.assertEqual(websiteURL + "charities", driver.current_url)
-	# 	driver.close()
+	# Tests the links on the home page
+	def test_homepage_links(self) :
+		websiteURL = self.websiteURL
+		driver = self.driver
+		link = driver.find_element_by_link_text('Find charities now')
+		link.click()
+		self.driver.implicitly_wait(30)
+		self.assertEqual(websiteURL + "charities", driver.current_url)
+		driver.close()
 
-	# # Tests the links on the about page
-	# def test_about_page_links(self) :
-	# 	driver = self.driver
-	# 	driver.find_element_by_link_text('About').click()
-	# 	driver.implicitly_wait(30)
-	# 	driver.find_element_by_link_text('Postman').click()
-	# 	driver.implicitly_wait(30)
-	# 	self.assertEqual("https://documenter.getpostman.com/view/4692440/RWMBSAp3", driver.current_url)
-	# 	driver.close()
+	# Tests the links on the about page
+	def test_about_page_links(self) :
+		driver = self.driver
+		driver.find_element_by_link_text('About').click()
+		driver.implicitly_wait(30)
+		driver.find_element_by_link_text('Postman').click()
+		driver.implicitly_wait(30)
+		self.assertEqual("https://documenter.getpostman.com/view/4692440/RWMBSAp3", driver.current_url)
+		driver.close()
 
 
-	# def test_about_page_links_2(self) :
-	# 	driver = self.driver
-	# 	driver.find_element_by_link_text('About').click()
-	# 	driver.implicitly_wait(30)
-	# 	driver.find_element_by_link_text('Gitbook').click()
-	# 	driver.implicitly_wait(30)
-	# 	self.assertEqual("https://knowyourtreatment.gitbook.io/project", driver.current_url)
-	# 	driver.close()
+	def test_about_page_links_2(self) :
+		driver = self.driver
+		driver.find_element_by_link_text('About').click()
+		driver.implicitly_wait(30)
+		driver.find_element_by_link_text('Gitbook').click()
+		driver.implicitly_wait(30)
+		self.assertEqual("https://knowyourtreatment.gitbook.io/project", driver.current_url)
+		driver.close()
 
-	# # Test sample of a link in the charities page
-	# def test_charities(self) :
-	# 	driver = self.driver
-	# 	driver.find_element_by_link_text('Charities').click()
-	# 	driver.find_element_by_link_text('RABIES IN THE AMERICAS').click()
-	# 	driver.find_element_by_link_text('Rabies').click()
-	# 	self.assertEqual('http://knowyourtreatment.com/healthconditions/Rabies', driver.current_url)
-	# 	driver.close()
+	# Test sample of a link in the charities page
+	def test_charities(self) :
+		driver = self.driver
+		driver.find_element_by_link_text('Charities').click()
+		driver.find_element_by_link_text('RABIES IN THE AMERICAS').click()
+		driver.find_element_by_link_text('Rabies').click()
+		self.assertEqual('http://knowyourtreatment.com/healthconditions/Rabies', driver.current_url)
+		driver.close()
 
-	# # Test sample of a link in the health conditions page
-	# def test_health_conditions(self) :
-	# 	driver = self.driver
-	# 	driver.find_element_by_link_text('Health Conditions').click()
-	# 	driver.find_element_by_link_text('Rabies').click()
-	# 	driver.find_element_by_link_text('Rabies vaccine').click()
-	# 	self.assertEqual('http://knowyourtreatment.com/medications/Rabies%20vaccine', driver.current_url)
-	# 	driver.close()
+	# Test sample of a link in the health conditions page
+	def test_health_conditions(self) :
+		driver = self.driver
+		driver.find_element_by_link_text('Health Conditions').click()
+		driver.find_element_by_link_text('Rabies').click()
+		driver.find_element_by_link_text('Rabies vaccine').click()
+		self.assertEqual('http://knowyourtreatment.com/medications/Rabies%20vaccine', driver.current_url)
+		driver.close()
 
-	# # Test sample of a link in the medications page
-	# def test_medications(self) :
-	# 	driver = self.driver
-	# 	driver.find_element_by_link_text('Medications').click()
-	# 	driver.find_element_by_link_text('Rabies vaccine').click()
-	# 	driver.find_element_by_link_text('Rabies').click()
-	# 	self.assertEqual('http://knowyourtreatment.com/healthconditions/Rabies', driver.current_url)
-	# 	driver.close()
+	# Test sample of a link in the medications page
+	def test_medications(self) :
+		driver = self.driver
+		driver.find_element_by_link_text('Medications').click()
+		driver.find_element_by_link_text('Rabies vaccine').click()
+		driver.find_element_by_link_text('Rabies').click()
+		self.assertEqual('http://knowyourtreatment.com/healthconditions/Rabies', driver.current_url)
+		driver.close()
 
 	# Test pagination
 	def test_pagination(self) :
