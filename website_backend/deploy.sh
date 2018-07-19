5b04b8ea2af61de1e06588f213cced52b2e8777a
brendan miller <brendanmiller.ca@gmail.com>
2018-07-18 20:37:04 -0500

    added an init script for the db setup

--- website_backend/deploy.sh
+++ website_backend/deploy.sh
@@ -3,4 +3,5 @@ docker container stop $(docker ps -q)
 docker system prune
 docker build -t backend .
 docker run -dit -v /home/ec2-user/website_backend/app:/app -w /app -p 80:80 -t backend:latest
-docker exec -dit $(docker ps -q) python app.py
+docker container ls
+docker exec -dit $(docker ps -q) /bin/bash ./init.sh
