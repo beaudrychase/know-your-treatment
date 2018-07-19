8d93306776af82f96a1257c9e1802f0264f33e45
Travis Llado <travisllado@utexas.edu>
2018-07-19 16:55:01 -0500

    cleaning up Search

--- website_frontend/app/components/Search.js
+++ website_frontend/app/components/Search.js
@@ -3,23 +3,20 @@ import { Link } from 'react-router-dom';
 import { Button, Input, ButtonGroup } from 'reactstrap';
 
 export default class Search extends React.Component {
-    
     constructor(props) {
-
         super(props);
 
         this.state = {
-            text: props.match.params.name,
-            charityResults: [],
-            diseaseResults: [],
-            treatmentResults: [],
-            completeResults: [],
-            displayCharity: true,
-            displayDisease: true,
-            displayTreatment: true,
-            natSort: true
+            text:               props.match.params.name,
+            charityResults:     [],
+            diseaseResults:     [],
+            treatmentResults:   [],
+            completeResults:    [],
+            displayCharity:     true,
+            displayDisease:     true,
+            displayTreatment:   true,
+            natSort:            true
         };
-
         /*console.log('params: ' + props.match.params.name);*/
 
         this.makeQueries = this.makeQueries.bind(this);
@@ -36,49 +33,38 @@ export default class Search extends React.Component {
         stateName -- state field to put queries in
     */
 
-    makeQueries(keyArray, fieldArray) {
-        var queries = keyArray.map( function( word ) {
-
-                                            return fieldArray.map( function( field ) {
-
-                                                                        return '{"name":"' + field + '","op":"like","val":"%' + word + '%"}';
-
-                                                                    });
-
-                                         });
+    makeQueries(keyArray, fieldArray){
+        var queries = keyArray.map( function( word ){
+            return fieldArray.map( function( field ){
+                return '{"name":"' + field + '","op":"like","val":"%' + word + '%"}';
+            });
+        });
 
         /* form the query url */
 
         return queries;
-
     }
-
 
     /* combines queries given by makeQueries into final query URL */
     /* modelName -- name of model to make search for (model defined by backend) */
     /* queries -- array of query filters to concat onto the base url */
-    makeUrl(modelName, queries) {
-
+    makeUrl(modelName, queries){
         var base = 'http://api.knowyourtreatment.com/api/' + modelName + '?q={"filters":[{"or":[';
-
         var i;
-        for(i=0; i < queries.length; i++) {
 
-            if(i == (queries.length - 1)) {
-
+        for(i=0; i < queries.length; i++){
+            if(i == (queries.length - 1)){
                 base = base + queries[i];
             } 
-            else {
+            else{
                 base = base + queries[i] + ',';
             }
         }
 
         base = base + ']}]}';
-
         /*console.log(base);*/
 
         return base;
-
     }
 
     /* calls backend api and parses results into modelResults[] */
@@ -88,72 +74,61 @@ export default class Search extends React.Component {
         let results = [];
 
         if(search != '') {
-
             /* Search term implementation */
             let searchArr = search.split(" ");
             let url;
+
             switch(model) {
                 case 'charity':
                     url = this.makeUrl(model, this.makeQueries(searchArr, ['name', 'category', 'city', 'missionStatement', 'state']));
                     break;
-    
                 case 'disease':
                     url = this.makeUrl(model, this.makeQueries(searchArr, ['name', 'prevention', 'symptoms', 'transmission', 'treatment']));
                     break;
-    
                 case 'treatment':
                     url = this.makeUrl(model, this.makeQueries(searchArr, ['name', 'text', 'treatment_type']));
             }
     
-            console.log('Search url: ' + url);
+            // console.log('Search url: ' + url);
     
             /* call back end and get results */
             fetch(url)
-            .then(results => { return results.json(); })
+            .then(results => results.json())
             .then(data => {
                 /*console.log(data);*/
-                let resArr = data.objects.map((o) => {
-                    return o.name;
-                });
+                let resArr = data.objects.map((o) => o.name);
                 let pages = data.total_pages;
                 let i;
-                if(pages == 1) {
+
+                if(pages == 1){
                     this.setState({ [model + 'Results']: resArr});
-                } else {
+                }
+                else{
                     for(i=2; i<=pages; i++) {
                         fetch(url + '&page=' + i)
-                        .then(res => { return res.json(); })
+                        .then(res => res.json())
                         .then(dat => {
-                            Array.prototype.push.apply(resArr, dat.objects.map((o) => {
-                                return o.name;
-                            }));
-                            console.log('Current complete array: ' + resArr);
+                            Array.prototype.push.apply(resArr, dat.objects.map((o) => o.name));
+                            // console.log('Current complete array: ' + resArr);
                             /*console.log('i iteration: ' + i);*/
                             this.setState({[model + 'Results']: resArr});
                         });
-                    }
-                    
+                    }                    
                 }
-                
-    
-                
             });
         }
-        
     }
 
-    componentWillMount() {
+    componentWillMount(){
         this.performSearch('charity');
         this.performSearch('disease');
         this.performSearch('treatment');
     }
 
     changeFilter(model) {
-
         let disp = this.state['display' + model];
         disp = !disp;
         this.setState({['display' + model]: disp});
-
     }
 
     sort() {
@@ -175,14 +150,11 @@ export default class Search extends React.Component {
         ns = !ns;
 
         this.setState({charityResults: cArr, diseaseResults: dArr, treatmentResults: tArr, natSort: ns});
-
     }
 
-    render() {
-
+    render(){
         return (
-                <div>
-
+            <div>
                 {/* filters */}
                 <p>Filter by Type</p>
                 <ButtonGroup>
@@ -197,25 +169,20 @@ export default class Search extends React.Component {
                 <p>{((this.state.text == '') || ((this.state.charityResults == []) && (this.state.diseaseResults == []) && (this.state.treatmentResults == []))) ? "No Results" : "Search Results"}</p>
 
                 <p>{(this.state.charityResults == []) ? "" : "Charities"}</p>
-
                 {((this.state.charityResults == []) || (this.state.displayCharity == false)) ? "" : this.state.charityResults.map(function(name, index) {
                     return <p><Link key={index} to={'/charities/' + name}> {name} </Link></p>;
                 })}
 
                 <p>{(this.state.diseaseResults == []) ? "" : "Diseases"}</p>
-
                 {((this.state.diseaseResults == []) || (this.state.displayDisease == false)) ? "" : this.state.diseaseResults.map(function(name, index) {
                     return <p><Link key={index} to={'/healthconditions/' + name}> {name} </Link></p>;
                 })}
 
                 <p>{(this.state.treatmentResults == []) ? "" : "Medicines"}</p>
-
                 {((this.state.treatmentResults == []) || (this.state.displayTreatment == false)) ? "" : this.state.treatmentResults.map(function(name, index) {
                     return <p><Link key={index} to={'/medications/' + name}> {name} </Link></p>;
                 })}
-
-
-                </div>
+            </div>
         );   
     }
 }
