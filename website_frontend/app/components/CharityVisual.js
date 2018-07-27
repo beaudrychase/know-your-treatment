5e8e9f91f5f0c68c697f3fecc91b6b15950d986d
Christopher Sauceda <csauce@utexas.edu>
2018-07-27 05:27:14 -0500

    Visualization anims / custom
    Transitions on the collage, color changes

--- website_frontend/app/components/CharityVisual.js
+++ website_frontend/app/components/CharityVisual.js
@@ -7,20 +7,31 @@ export default class CharityVisual extends React.Component {
         super(props)
         this.state = {
             data: [],
-            images: ['boi', 'hey'],
-            test: 'TEST'
+            dict: [],
+            images: [],
+            timer: null
         }
 
         this.fetchImages = this.fetchImages.bind(this)
         this.makeChart = this.makeChart.bind(this)
+        this.makeCollage = this.makeCollage.bind(this)
+        this.collageAnim = this.collageAnim.bind(this)
+        this.getRandomInt = this.getRandomInt.bind(this)
     }
 
     componentDidMount() {
         this.makeChart()
+        var t = setInterval(this.collageAnim, 500)
+	  	this.setState({timer: t})
     }
 
     componentDidUpdate() {
-        this.makeChart()
+    	this.makeChart()
+    	this.makeCollage()
+    }
+
+    componentWillUnmount() {
+    	clearInterval(this.state.timer)
     }
 
     fetchImages(apiName) {
@@ -42,11 +53,83 @@ export default class CharityVisual extends React.Component {
                 })
                 .then(img => {
                     Array.prototype.push.apply(images, img)
-                    this.setState({images: images})
                 })
+                this.setState({images: images})
             }
             this.setState({images: images})
-        });
+        })
+        
+    }
+
+    collageAnim() {
+    	const canv = this.canvas
+    	var imgArr = d3.select(canv).selectAll('image').nodes()
+    	var i = this.getRandomInt(0, imgArr.length)
+    	var op = this.getRandomInt(0, 100) / 100
+
+    	imgArr[i].parentElement.appendChild(imgArr[i])
+
+    	d3.select(imgArr[i])
+    	  .transition()
+    	  .duration(2000)
+    	  .ease(d3.easeElastic)
+    	  .attr('x', this.getRandomInt(0, 300))
+    	  .attr('y', this.getRandomInt(0, 300))
+    	  .style('opacity', op)
+    }
+
+    makeCollage() {
+    	const canv = this.canvas
+    	d3.select(canv)
+    	  .selectAll('image')
+    	  .data(this.state.images)
+    	  .enter()
+    	  .append('svg:image')
+    	  	.attr('xlink:href', d => d)
+    	  	.attr('x', (d, i) => i * 30)
+    	  	.attr('y', (d, i) => i * 30)
+    	  	.attr('width', 200)
+    	  	.attr('height', 200)
+    }
+
+    getRandomInt(min, max) {
+	  min = Math.ceil(min)
+	  max = Math.floor(max)
+	  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+	}
+
+	getRandomColor() {
+    	switch(this.getRandomInt(0, 9)) {
+    		case 0:
+    		return 'blue'
+    		break
+    		case 1:
+    		return 'indigo'
+    		break
+    		case 2:
+    		return 'purple'
+    		break
+    		case 3:
+    		return 'pink'
+    		break
+    		case 4:
+    		return 'red'
+    		break
+    		case 5:
+    		return 'orange'
+    		break
+    		case 6:
+    		return 'yellow'
+    		break
+    		case 7:
+    		return 'green'
+    		break
+    		case 8:
+    		return 'teal'
+    		break
+    		case 9:
+    		return 'cyan'
+    	}
     }
 
     makeChart() {
@@ -58,15 +141,22 @@ export default class CharityVisual extends React.Component {
         .selectAll('rect')
         .data(this.state.data)
         .enter()
+
         .append('rect')
             .attr('width', 30)
-            .attr('height', r => scale(r))
             .attr('x', (r, i) => i * 30)
             .attr('y', r => 500 - scale(r))
-            .style('fill', 'green')
+            .style('fill', this.getRandomColor())
             .style('stroke', 'black')
             .style('stroke-width', '1px')
+            .transition()
+            .ease(d3.easeBounce)
+            .duration(2000)
+            .attr('height', r => scale(r))
+
     }
+
+
 
     componentWillMount() {
         this.fetchImages('treatment')
@@ -112,10 +202,10 @@ export default class CharityVisual extends React.Component {
                             occurrences.push(1)
                         }
                     })
-                    this.setState({data:occurrences})
+                    this.setState({data: occurrences, dict: dictionary})
                 })
             }
-            this.setState({data:occurrences})
+            this.setState({data: occurrences, dict: dictionary})
         })
         .then(data => this.makeChart())
 
@@ -124,21 +214,23 @@ export default class CharityVisual extends React.Component {
     render() {
         return(
             <div>
-                <h1>Number of Charities per Condition </h1>
+                <h1>Number of Charities per Category </h1>
                 <svg ref={node => this.node = node} width={500} height={500} style={{border: '1px solid'}}></svg>
+
+                <h1>Image Collage </h1>
+                <svg ref={canvas => this.canvas = canvas} width={500} height={500} style={{border: '1px solid'}}></svg>
 
                 <br /><br />
 
                 <table width="720">
-                    <tr align="center"><td colspan="4"><h1>Commits per Author, per Project Phase</h1></td></tr>
-                    <tr><td colspan="4"><img src={require("./images/chart_commits.png")} width="600" /></td></tr>
+                	<tbody>
+                    <tr align="center"><td colSpan="4"><h1>Commits per Author, per Project Phase</h1></td></tr>
+                    <tr><td colSpan="4"><img src={require("./images/chart_commits.png")} width="600" /></td></tr>
                     <tr><td>Phase 1</td><td>Phase 2</td><td>Phase 3</td><td>Phase 4</td></tr>
+                    </tbody>
                 </table>
 
                 <br /><br />
-
-                <h1>Associated Media</h1>
-                <div>{this.state.images && this.state.images.map(i => <img src={i} />)}</div>
             </div>
         )
     }
